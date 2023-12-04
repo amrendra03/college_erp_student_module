9fe2a5f242f4dac163109ecb90e960f7c9edf7b0
amrendra03 <amrendra9076893685@gmail.com>
2023-12-05 01:01:18 +0530

    dashboard-ui-implementing

--- client/src/dashboard/Dashboard.jsx
+++ client/src/dashboard/Dashboard.jsx
@@ -3,12 +3,151 @@ import Header from '../component/header/Header';
 import './dashboard.css';
 
 const Dashboard = () => {
+
+   const data = [
+      { subject: 'Math', faculty: 'Dr. Smith', present: 8, absent: 2, totalAssignment: 15, submitted: 12, notSubmitted: 3 },
+      { subject: 'English', faculty: 'Prof. Johnson', present: 9, absent: 1, totalAssignment: 12, submitted: 10, notSubmitted: 2 },
+      { subject: 'Science', faculty: 'Dr. Brown', present: 7, absent: 3, totalAssignment: 10, submitted: 8, notSubmitted: 2 },
+      { subject: 'History', faculty: 'Prof. Davis', present: 6, absent: 4, totalAssignment: 8, submitted: 7, notSubmitted: 1 },
+      { subject: 'Geography', faculty: 'Dr. Wilson', present: 8, absent: 2, totalAssignment: 12, submitted: 9, notSubmitted: 3 },
+      { subject: 'Computer Science', faculty: 'Prof. Miller', present: 7, absent: 3, totalAssignment: 10, submitted: 8, notSubmitted: 2 },
+      { subject: 'Art', faculty: 'Dr. Turner', present: 9, absent: 1, totalAssignment: 15, submitted: 12, notSubmitted: 3 },
+      { subject: 'Physical Education', faculty: 'Prof. Clark', present: 8, absent: 2, totalAssignment: 12, submitted: 10, notSubmitted: 2 },
+      { subject: 'Music', faculty: 'Dr. Harris', present: 6, absent: 4, totalAssignment: 8, submitted: 7, notSubmitted: 1 },
+      { subject: 'Language', faculty: 'Prof. Robinson', present: 7, absent: 3, totalAssignment: 10, submitted: 8, notSubmitted: 2 },
+   ];
+   const notifi = [
+      { message: "Final exam date", active: 0 },
+      { message: "Project submission deadline", active: 0 },
+      { message: "Club meeting tomorrow", active: 0 },
+      { message: "Library closed for renovations", active: 0 },
+      { message: "Guest lecture on Friday", active: 0 },
+      { message: "Reminder: Pay tuition fees", active: 1 },
+      { message: "Sports day announcement", active: 1 },
+      { message: "Holiday schedule update", active: 1 },
+      { message: "Career fair next week", active: 1 },
+      { message: "Book return deadline", active: 1 },
+   ];
+
+
+
    return (
       <div className='dashboard' >
          <Header />
+         <div className='ds-1'>
+            <div className="Status1 display" >
+               <div className='st-icon' />
+               <div>
+                  <span className='txt'>Amrendra</span><br />
+                  <span className='txt'>26546465688798</span>
+               </div>
+            </div>
+            <div className="Status2 display" >
+               <div className='st-icon-2' />
+               <div>
+                  <span className='txt'>B.Tech CSE( AI &ML)</span><br />
+                  <span className='txt'>sem 7th</span>
+               </div>
+            </div>
+            <div className="Status3 display" >
+               <div className='st-icon-3' />
+               <div>
+                  <span className='txt'>Fee</span><br />
+                  <span className='txt'>1,00,000</span>
+               </div>
+            </div>
+            <div className="Status4 display" >
+               <div className='st-icon-4' />
+               <div>
+                  <span className='txt'>Fine</span><br />
+                  <span className='txt'>1,00,000</span>
+               </div></div>
+            <div className="Status5 display" >
+               <div className='st-icon-5' />
+               <div>
+                  <span className='txt'>Comptetive Score</span><br />
+                  <span className='txt'>26546465688798</span>
+               </div>
+            </div>
+         </div>
+         <div className='ds-2'>
+            <div className='ds-3'>
+               <div className='ds-5'>
+                  <div className='thead'>
+                     <div className='cl-1' style={{ width: 160 }}>Subject</div>
+                     <div className='cl-1' style={{ width: 150 }}>Faculty</div>
+                     {/* <div className='cl-1' style={{ width: 50 }}>Total</div> */}
+                     <div className='cl-1' style={{ width: 70 }}>Present</div>
+                     <div className='cl-1' style={{ width: 70 }}>Absent</div>
+                     <div className='cl-1' style={{ width: 120 }}>Total Assingment</div>
+                     <div className='cl-1' style={{ width: 90 }}>Submitted</div>
+                     <div className='cl-1' style={{ width: 110, border: 'none' }}>Not Submitted</div>
+                  </div>
+                  <div className='ds-6' id='t-row' >
+                     {data.map((item, index) => (
+                        <Row key={index} data={item} />
+                     ))}
+                  </div>
+               </div>
+               <div className='notifi-1'>
+                  <div className='notifi-2'>
+                     <span style={{ marginLeft: 10 }}>
+                        Notification
+                     </span>
+                  </div>
+                  <div className='notifi-3'>
+                     {notifi.map((item, index) => (
+                        <Notifi key={index} data={item} />
+                     ))}
+                  </div>
+               </div>
+            </div>
+            <div className='ds-4'>
+
+               <div className='ds-hex'>
+
+               </div>
+               <div className='ds-cal'>
+
+               </div>
+
+            </div>
+         </div>
 
       </div>
    )
 }
+
+
+const Row = ({ data }) => {
+   return (
+      <div className='ds-6-row-1'>
+         <div className='row-1' style={{ width: 160 }}>{data.subject}</div>
+         <div className='row-1' style={{ width: 150 }}>{data.faculty}</div>
+         <div className='row-1' style={{ width: 70 }}>{data.present}</div>
+         <div className='row-1' style={{ width: 70 }}>{data.absent}</div>
+         <div className='row-1' style={{ width: 120 }}>{data.totalAssignment}</div>
+         <div className='row-1' style={{ width: 90 }}>{data.submitted}</div>
+         <div className='row-1' style={{ width: 110, border: 'none' }}>{data.notSubmitted}</div>
+      </div>
+   );
+};
+
+const Notifi = ({ data }) => {
+   const notifiClass = data.active === 1 ? 'notifi-icon-img-d' : 'notifi-icon-img-h';
+   const notirow = data.active === 1 ? 'notifi-row-color' : '';
+   const notimess = data.active === 1 ? 'notifi-row-mess' : '';
+
+   return (
+      <>
+         <div className={`notifi-row ${notirow}`}>
+            <div className={`notifi-icon ${notifiClass}`} />
+            <div className={`notifi-text ${notimess}`}>{data.message}</div>
+         </div>
+      </>
+   )
+
+}
+
 
 export default Dashboard
