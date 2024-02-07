480f07f3ac88df3c6d16497417f20550388d6807
amrendra03 <amrendra9076893685@gmail.com>
2024-02-07 21:49:20 +0530

    log4j implementing

--- server/src/main/java/com/server/ServerApplication.java
+++ server/src/main/java/com/server/ServerApplication.java
@@ -1,16 +1,19 @@
 package com.server;
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
+	private static  final Logger LOG = LoggerFactory.getLogger(ServerApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
-		System.out.println("server started...");
+		LOG.info("Server Started......");
 	}
 
 	@Bean
--- server/src/main/java/com/server/config/CorsConfig.java
+++ server/src/main/java/com/server/config/CorsConfig.java
@@ -14,14 +14,12 @@ public class CorsConfig {
       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
       CorsConfiguration config = new CorsConfiguration();
 
-      // Allow all origins, headers, and methods. You may want to customize this based
+      // allow all origins, headers, and methods. You may want to customize this based
       // on your requirements.
       config.addAllowedOriginPattern("*");
       config.addAllowedHeader("*");
       config.addAllowedMethod("*");
 
-      // Allow credentials. Set this to true if your API and client are on different
-      // domains and you need to include credentials (e.g., cookies).
       config.setAllowCredentials(true);
 
       source.registerCorsConfiguration("/**", config);
